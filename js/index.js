a2b91f7084b9ebe81d3e3eddfd0af0d0a3dcbec4
yoshfoydalanuvchi <yoshfoydalanuvchi@gmail.com>
2024-06-08 16:32:01 +0500

    none

---
+++ js/index.js
@@ -0,0 +1,146 @@
+// ternary operatoriga oid
+
+// 1-masala
+
+// let a = +prompt("son kiriting");
+// let result = a > 0 ? "positive" : "minus";
+// alert(result);
+
+// 2-masala
+
+// let a = +prompt("son kiriting");
+// let b = +prompt("yana bitta son kiriting");
+// let result = a > b ? "birinchi son katta" : "ikkinchi son katta";
+// alert(result);
+
+// 4-masala
+
+// let a = +prompt("son kiriting");
+// let result = a % 2 == 0 ? "juft" : "toq";
+// alert(result);
+
+// 5-masala
+
+// let a = +prompt("son kiriting");
+// let result = a > 0 ? "positive" : "minus";
+// alert(result);
+
+// 1-masala
+// let a = +prompt("son");
+// switch (a) {
+//   case 1:
+//     alert("dushanba");
+//     break;
+//   case 2:
+//     alert("seshanba");
+//     break;
+//   case 3:
+//     alert("chorshanba");
+//     break;
+//   case 4:
+//     alert("pashanba");
+//     break;
+//   case 5:
+//     alert("juma");
+//     break;
+//   case 6:
+//     alert("shanba");
+//     break;
+//   case 7:
+//     alert("yakshanba");
+//     break;
+//   default:
+//     alert("bunday hafta kuni yoq");
+//     break;
+// }
+
+// 2-masala
+
+// let a = +prompt("son");
+// switch (a) {
+//   case 12:
+//   case 1:
+//   case 2:
+//     alert("qish");
+//     break;
+//   case 3:
+//   case 4:
+//   case 5:
+//     alert("bahor");
+//     break;
+//   case 6:
+//   case 7:
+//   case 8:
+//     alert("yoz");
+//     break;
+//   case 9:
+//   case 10:
+//   case 11:
+//     alert("kuz");
+//     break;
+// }
+
+// 3-masala
+
+// let a = +prompt("son");
+// switch (a) {
+//   case 1:
+//     alert("birinchi");
+//     break;
+//   case 2:
+//     alert("ikkinchi");
+//     break;
+//   case 3:
+//     alert("uchinchi");
+//     break;
+//   case 4:
+//     alert("tochinchi");
+//     break;
+//   case 5:
+//     alert("beshinchi");
+//     break;
+//   case 6:
+//     alert("oltinchi");
+//     break;
+//   case 7:
+//     alert("yettinchi");
+//     break;
+//   case 8:
+//     alert("sakkizinchi");
+//   case 9:
+//     alert("toqqizinchi");
+//     break;
+//   case 10:
+//     alert("oninchi");
+//     break;
+//   case 11:
+//     alert("onbirinchi");
+//     break;
+//   case 12:
+//     alert("onikkinchi");
+//     break;
+//   default:
+//     alert("xato");
+//     break;
+// }
+
+// 1-masala
+
+// let kod = "kod yozing";
+// for (let i = 1; i <= 10; i++) {
+//   alert(i * i);
+// }
+
+// 2-masala
+
+// let i = prompt("kod yozing");
+// for (let i = 10; i >= 1; i--) {
+//   alert(i);
+// }
+
+// 4-masala
+
+// let i = 0;
+// for (let i = 0; i <= 20; i += 2) {
+//   alert(i);
+// }
